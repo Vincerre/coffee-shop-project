ffad01f1e384e3ac790b0a64442d388eba479859
Patryk Bazior <bazior.patryk@gmail.com>
2023-03-07 15:04:05 +0100

    splash header

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,76 @@
-import { settings } from 'settings.js';
-
+import { settings, select, classNames } from './settings.js';
+import Product from './components/Product.js';
+import Home from './components/Home.js';
 const app = {
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    thisApp.activatePage(idFromHash);
+    const idFromHash = window.location.hash.replace('#/', '');
+    // eslint-disable-next-line no-unused-vars
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+    }
+  },
+
+  initHome: function () {
+    const homeContainer = document.querySelector(select.containerOf.home);
+    new Home(homeContainer);
+  },
+
+  initMenu: function () {
+    const thisApp = this;
+    for (let productData in thisApp.data.products) {
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+  },
+
   initData: function () {
+    const thisApp = this;
     const url = settings.db.url + '/' + settings.db.products;
-    this.data = {};
+    thisApp.data = {};
     fetch(url)
       .then((rawResponse) => {
         return rawResponse.json();
       })
       .then((parsedResponse) => {
-        this.data.products = parsedResponse;
+        thisApp.data.products = parsedResponse;
+        thisApp.initMenu();
       });
   },
 
   init: function () {
     const thisApp = this;
     thisApp.initData();
+    thisApp.initHome();
+    thisApp.initPages();
   },
 };
 app.init();
---
+++ src/js/settings.js
@@ -0,0 +1,36 @@
+export const select = {
+  templateOf: {
+    homeWidget: '#template-home-widget',
+    menuProduct: '#template-menu-product',
+  },
+  containerOf: {
+    menu: '#product-list',
+    pages: '#pages',
+    home: '.home-wrapper',
+  },
+  nav: {
+    links: '.nav-bar-right a',
+  },
+};
+
+export const classNames = {
+  menuProduct: {
+    wrapperActive: 'active',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
+};
+export const settings = {
+  db: {
+    url: '//' + window.location.hostname + (window.location.hostname == 'localhost' ? ':3131' : ''),
+    products: 'products',
+  },
+};
+export const templates = {
+  menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+  homeWidget: Handlebars.compile(document.querySelector(select.templateOf.homeWidget).innerHTML),
+};
